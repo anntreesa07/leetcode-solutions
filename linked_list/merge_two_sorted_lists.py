d3b39d9f50b33d937cfbec59c217a7a401e3f885
Ann <anntreesa818@gmail.com>
2026-05-24 23:37:15 +0530

    Create merge_two_sorted_lists.py

---
+++ linked_list/merge_two_sorted_lists.py
@@ -0,0 +1,38 @@
+# 21. Merge Two Sorted Lists
+
+# We can do this by comparing the values of the nodes in both lists and adding the smaller value to the new list. We can use a dummy node to simplify the process of building the new list. 
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        
+
+        dummy = ListNode()
+        current = dummy
+
+        while list1 and list2:
+            if list1.val<list2.val:
+                current.next=list1   #take value
+
+                list1=list1.next     #move list1
+            else:
+                current.next=list2
+                list2=list2.next     #move list2
+
+            current=current.next
+
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2        
+
+        return dummy.next
+        
